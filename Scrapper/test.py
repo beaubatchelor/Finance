62963dc056bc54559f119540d4cbdbf690e56285
Beau Batchelor <beauwbatchelor@gmail.com>
2019-03-03 23:58:02 -0800

    adding xbrl to dict itterator

--- Scrapper/test.py
+++ Scrapper/test.py
@@ -7,15 +7,14 @@ import requests
 import xml.etree.ElementTree as ET
 
 
+
 def filings_df(address):
 #     variables 
     reports_list = []
     doc_link = ''
     
 #     request
-    resp = requests.get(address)
-    html_str = resp.text
-    soup = BeautifulSoup(html_str, 'html.parser')
+    soup = BeautifulSoup(address, 'html.parser')
     
 #     getting filings info info
     table_tag = soup.find('table', class_='tableFile2')
@@ -26,7 +25,7 @@ def filings_df(address):
         if len(cells) > 3:
             filing = cells[0].text
             doc_link = 'https://www.sec.gov' + cells[1].a['href']
-            filing_date = dt.date.fromisoformat(cells[3].text[0:10])
+            filing_date = dt.date.fromisoformat(cells[3].text)
             entry = {'filings' : filing, 'doc_link' : doc_link, 'date' : filing_date}
             reports_list.append(entry)
     
@@ -39,7 +38,7 @@ def filings_df(address):
         try:
             table_tag = soup.find('table', class_='tableFile', summary='Data Files')
             rows = table_tag.find_all('tr')
-            time.sleep(.1)
+            time.sleep(.01)
             for row in rows:
                 cells = row.find_all('td')
                 if len(cells) > 3:
@@ -53,21 +52,4 @@ def filings_df(address):
     
     filings_df = pd.DataFrame(reports_list)
     
-    return filings_df
-
-
-def filings_collector(cik):
-    establishing_url = f'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={cik}&type=&dateb=&owner=exclude&start=0&count=100'
-    establishing_df = filings_df(establishing_url)
-    for i in range(1,10):
-        try:
-            new_url = f'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={ticker}&type=&dateb=&owner=exclude&start={i}00&count=100'
-            page_df = filings_df(new_url)
-            establishing_df = establishing_df.append(page_df, ignore_index=True)
-        except:
-            break
-    return establishing_df
-
-
-
-
+    return filing_df
