67adb25bc5f9ade5a9f4bbcc8ac9d4c31787750c
Beau Batchelor <beauwbatchelor@gmail.com>
2019-04-16 19:54:10 -0700

    found out that zillow doesn't let you scrape their data... shocker

---
+++ real_estate/code/scrapper/zillow_scrapper.py
@@ -0,0 +1,3 @@
+
+
+los_angeles_path = 'https://www.zillow.com/homes/?searchQueryState={%22mapBounds%22:{%22west%22:-118.93426973144528,%22east%22:-117.88919526855466,%22south%22:33.560555702306964,%22north%22:34.47909983375622},%22isMapVisible%22:false,%22filterState%22:{},%22isListVisible%22:true}'
